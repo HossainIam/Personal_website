2cf1435946cfa2712cbbfe8caffae112a2a0d380
fhossain0 <farhadbks3@gmail.com>
2025-11-26 23:06:36 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__) # Application instance
+
+@app.route("/")
+def home():
+    return render_template("home.html")  # Flask loads templates/index.html
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/education")
+def education():
+    return render_template("education.html")
+
+if __name__ == "__main__":
+    app.run(debug=True, port = 5001)
