c9cf50039e5cc8075b2f992595761e7adef8c987
fhossain0 <farhadbks3@gmail.com>
2025-12-08 03:30:28 +0100

    Highlight

--- static/script.js
+++ static/script.js
@@ -17,3 +17,27 @@ const observer = new IntersectionObserver(entries => {
 document.querySelectorAll(".fade-in").forEach(el => observer.observe(el));
 
 console.log("JS loaded!"); // to check the jav script loading or not
+
+
+// Highlight based on scroll position
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("nav a");
+
+window.addEventListener("scroll", () => {
+    let current = "";
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        if (scrollY >= sectionTop - 80) {
+            current = section.getAttribute("id");
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove("active");
+        if (link.getAttribute("href") === "#" + current) {
+            link.classList.add("active");
+        }
+    });
+});
